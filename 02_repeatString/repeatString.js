713f79c9e89d8e5ce192043950da01467e6e4586
Ananta Gurung <anantar@live.com>
2022-03-12 17:23:43 -0500

    Wrote function to repeat a given string x amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedStr = '';
+    if (num < 0) {
+        return 'ERROR';
+    } for (i = 0; i <= num; i++) {
+        repeatedStr = str.repeat(num);
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
