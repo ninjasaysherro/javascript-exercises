7aff5b7816d1e85943a061c91627112dbf0b96df
Ananta Gurung <anantar@live.com>
2022-03-18 18:17:35 -0400

    Add two functions to convert farenheit to celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  f = parseFloat(f);
+  c = (f - 32) / 1.8
+  return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  c = parseFloat(c);
+  f = (c * 1.8) + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
